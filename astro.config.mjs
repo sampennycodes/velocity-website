ae0886483bbbf907421584e6836f9be0d3148ec3
Sam Penny <sam@Sams-MacBook-Air.local>
2025-08-14 13:29:43 +1000

    fix: Switch to Node adapter to resolve persistent Vercel entry.mjs module errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: node({
+    mode: 'standalone'
+  }),
   server: {
     port: 4321,
     host: true
